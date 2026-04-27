6378a8f70c23c4f703e0e38283ea27230469763e
Srihari2004123 <srihari0453@gmail.com>
2026-04-27 19:59:20 +0530

    changing ui design to 3d

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -116,7 +116,7 @@ const VisualizerId = () => {
                 <div className="brand">
                     <Box className="logo" />
 
-                    <span className="name">Roomify</span>
+                    <span className="name">RoomGenAi</span>
                 </div>
                 <Button variant="ghost" size="sm" onClick={handleBack} className="exit">
                     <X className="icon" /> Exit Editor
